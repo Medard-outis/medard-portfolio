e9c75e1e56d1f1e1570991f63b09fe8f917ca41f
medard <medardkik01@gmail.com>
2026-03-11 15:05:47 +0200

    Implement responsive navigation bar functionality in main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,14 @@
 // NAVIGATION BAR FUNCTION-------------
+// ----- RESPONSIVE NAVIGATION BAR
+function myMenuFunction() {
+    var menuBtn = document.getElementById("myNavMenu");
 
+    if(menuBtn.className === "nav-menu") {
+        menuBtn.className += " responsive";
+    } else{
+        menuBtn.className = "nav-menu";
+    }
+}
 
 // ----- ADD SHADOW ON NAVIGATION BAR WHILE SCROLLING
 
